6593e6e289c67acf6a95a9a02aa4dc86df2b5a70
Shah-Developer-01 <ridertalbat9@gmail.com>
2026-03-13 14:49:26 +0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.label import Label
+class ShahApp(App):
+    def build(self):
+        return Label(text="SHAH MISSION SUCCESS!")
+if __name__ == "__main__":
+    ShahApp().run()
